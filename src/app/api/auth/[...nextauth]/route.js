0310850689e5aabadb64200179b0768ad3dd6662
Bruno Figueiredo <bruno8f@gmail.com>
2024-01-28 14:37:49 -0300

    Upload Login/Logout

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,51 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "f8bruno" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        console.log(credentials);
+
+        if (credentials?.username === "f8bruno@outlook.com") {
+          return {
+            name: "Bruno Figueiredo",
+            avatar: "BF",
+            email: "f8bruno@gmail.com",
+          };
+        }
+
+        return null;
+      },
+    }),
+  ],
+  session: {
+    jwt: true,
+    maxAge: 3600,
+  },
+  jwt: {
+    signingKey: process.env.JWT_SIGNING_PRIVATE_KEY,
+  },
+  callbacks: {
+    async session({ session, token }) {
+      session.user = token.user;
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user;
+      }
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export { handler as GET, handler as POST };
---
+++ src/app/login/page.tsx
@@ -0,0 +1,72 @@
+'use client';
+import {
+    TextInput,
+    PasswordInput,
+    Checkbox,
+    Anchor,
+    Paper,
+    Title,
+    Text,
+    Container,
+    Group,
+    Button,
+} from '@mantine/core';
+import classes from './login.module.css';
+import { useRouter } from 'next/navigation';
+import { signIn } from 'next-auth/react';
+
+export default function Page() {
+
+    const router = useRouter();
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        const form = new FormData(event.target);
+        console.log(form);
+        const username = form.get("username");
+        const password = form.get("password");
+
+        const result = await signIn("credentials", {
+            username,
+            password,
+            redirect: false,
+        });
+
+        if (!result.error) {
+            return router.push("/");
+        }
+
+        // TODO: handle error
+    };
+
+    return (
+        <form onSubmit={(e) => onSubmit(e)}>
+            <Container size={420} my={40}>
+                <Title ta="center" className={classes.title}>
+                    Welcome back!
+                </Title>
+                <Text c="dimmed" size="sm" ta="center" mt={5}>
+                    Do not have an account yet?{' '}
+                    <Anchor size="sm" component="button">
+                        Create account
+                    </Anchor>
+                </Text>
+
+                <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+                    <TextInput label="Email" placeholder="seu@email.com" required name="username" />
+                    <PasswordInput label="Password" placeholder="********" required mt="md" name="password" />
+                    <Group justify="space-between" mt="lg">
+                        <Checkbox label="Remember me" />
+                        <Anchor component="button" size="sm">
+                            Forgot password?
+                        </Anchor>
+                    </Group>
+                    <Button fullWidth mt="xl" type="submit">
+                        Login
+                    </Button>
+                </Paper>
+            </Container>
+        </form>
+    );
+}
