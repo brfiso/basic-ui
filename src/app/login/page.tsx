9a05716a736baefbca4630066e8fab77f73371d3
Bruno Figueiredo <bruno8f@gmail.com>
2024-01-30 18:58:01 -0300

    Correção do fluxo de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,17 +13,19 @@ import {
 } from '@mantine/core';
 import classes from './login.module.css';
 import { useRouter } from 'next/navigation';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
+import Loading from '../ui/Loading';
 
 export default function Page() {
 
     const router = useRouter();
+    const { data: session, status } = useSession();
 
     const onSubmit = async (event) => {
         event.preventDefault();
 
         const form = new FormData(event.target);
-        console.log(form);
+
         const username = form.get("username");
         const password = form.get("password");
 
@@ -36,37 +38,42 @@ export default function Page() {
         if (!result.error) {
             return router.push("/");
         }
-
-        // TODO: handle error
     };
 
-    return (
-        <form onSubmit={(e) => onSubmit(e)}>
-            <Container size={420} my={40}>
-                <Title ta="center" className={classes.title}>
-                    Welcome back!
-                </Title>
-                <Text c="dimmed" size="sm" ta="center" mt={5}>
-                    Do not have an account yet?{' '}
-                    <Anchor size="sm" component="button">
-                        Create account
-                    </Anchor>
-                </Text>
+    if (status === "loading")
+        return (<Loading />);
+    else if (status === 'authenticated')
+        return router.push("/dashboard");
+    else {
 
-                <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-                    <TextInput label="Email" placeholder="seu@email.com" required name="username" />
-                    <PasswordInput label="Password" placeholder="********" required mt="md" name="password" />
-                    <Group justify="space-between" mt="lg">
-                        <Checkbox label="Remember me" />
-                        <Anchor component="button" size="sm">
-                            Forgot password?
+        return (
+            <form onSubmit={(e) => onSubmit(e)}>
+                <Container size={420} my={40}>
+                    <Title ta="center" className={classes.title}>
+                        Welcome back!
+                    </Title>
+                    <Text c="dimmed" size="sm" ta="center" mt={5}>
+                        Do not have an account yet?{' '}
+                        <Anchor size="sm" component="button">
+                            Create account
                         </Anchor>
-                    </Group>
-                    <Button fullWidth mt="xl" type="submit">
-                        Login
-                    </Button>
-                </Paper>
-            </Container>
-        </form>
-    );
+                    </Text>
+
+                    <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+                        <TextInput label="Email" placeholder="seu@email.com" required name="username" />
+                        <PasswordInput label="Password" placeholder="********" required mt="md" name="password" />
+                        <Group justify="space-between" mt="lg">
+                            <Checkbox label="Remember me" />
+                            <Anchor component="button" size="sm">
+                                Forgot password?
+                            </Anchor>
+                        </Group>
+                        <Button fullWidth mt="xl" type="submit">
+                            Login
+                        </Button>
+                    </Paper>
+                </Container>
+            </form>
+        );
+    }
 }
